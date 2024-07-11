a2b978eaf1ec7186026e475ec21b0b11840009e5
EternalRex <karlevemarx@gmail.com>
2024-07-11 10:27:56 +0800

    fix landing page errors

--- src/src/index.tsx
+++ src/src/index.tsx
@@ -3,11 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {
-  RouterProvider,
-  BrowserRouter,
-  createBrowserRouter,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import About from "./pages/aboutpage/About";
 import Guides from "./pages/guidespage/Guides";
 
