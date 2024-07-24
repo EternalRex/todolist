77d650defe5ae3504d50104b2d99c236cf6fedd1
EternalRex <karlevemarx@gmail.com>
2024-07-24 09:18:41 +0800

    fixing pipeline node.js config3

--- todolistapp/app/homepage/layout.tsx
+++ todolistapp/app/homepage/layout.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export default function ({ children }: { children: React.ReactNode }) {
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <main>
       <p>Root Layout</p>
